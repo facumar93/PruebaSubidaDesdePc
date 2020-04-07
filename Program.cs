bab908315565547311b66bf75c3de1060914bf46
facumar93 <53872293+facumar93@users.noreply.github.com>
2020-04-07 20:51:22 -0300

    CambioEnRama...

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CreandoCodigoHaciendoloRepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello People!");
         }
     }
 }
